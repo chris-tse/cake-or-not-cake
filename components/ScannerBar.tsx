85c857fbb22e5f72b80398356c88783d63da3d97
Christopher Tse <chris@chris-tse.com>
2020-07-12 21:54:00 -0500

    Add animation as jsx style to dynamically change height of animation

--- components/ScannerBar.tsx
+++ components/ScannerBar.tsx
@@ -1,13 +1,35 @@
-export default function ScannerBar({ classes }) {
+type ScannerBarProps = {
+    classes: string
+    height: number
+}
+
+export default function ScannerBar({ classes, height }) {
     return (
-        <div
-            className={`scanner ${classes}`}
-            style={{
-                maxWidth: 350,
-                height: 0,
-                background: 'green',
-                boxShadow: '0 0 10px 5px #0f0',
-            }}
-        ></div>
+        <>
+            <style jsx>{`
+                .scanner {
+                    transform: translateY(0px);
+                    animation: scanning 2s ease-in-out infinite;
+                }
+
+                @keyframes scanning {
+                    50% {
+                        transform: translateY(${height}px);
+                    }
+                    100% {
+                        transform: translateY(0px);
+                    }
+                }
+            `}</style>
+            <div
+                className={`scanner ${classes}`}
+                style={{
+                    maxWidth: 350,
+                    height: 0,
+                    background: 'green',
+                    boxShadow: ' 0 0 1px 1px #0f0, 0 0 15px 3px #0f0',
+                }}
+            ></div>
+        </>
     )
 }
